8c0f55ce8080cb25a3593b333b9cef9a4d7ad1fb
Swamiom3 <106953535+Swamiom3@users.noreply.github.com>
2022-06-23 13:57:58 +0530

    Add files via upload

---
+++ Vowelornot.java
@@ -0,0 +1,76 @@
+import java.util.*;
+class Vowelornot
+{
+public static void main(String[]args)
+{
+Scanner sc=new Scanner(System.in);
+
+System.out.println("ENTER YOUR CHARCTER");
+char n=sc.next().charAt(0);
+
+switch(n)
+{
+case 'a':
+{
+System.out.println("\n\n\t\t\tTHIS IS VOWEL");
+System.out.println("\n\n\t\t\t==============");
+break;
+}
+case 'e':
+{
+System.out.println("\n\n\t\t\tTHIS IS VOWEL");
+System.out.println("\n\n\t\t\t==============");
+break;
+}
+case 'i':
+{
+System.out.println("\n\n\t\t\tTHIS IS VOWEL");
+System.out.println("\n\n\t\t\t==============");
+break;
+}
+case 'o':
+{
+System.out.println("\n\n\t\t\tTHIS IS VOWEL");
+System.out.println("\n\n\t\t\t==============");
+break;
+}
+case 'u':
+{
+System.out.println("\n\n\t\t\tTHIS IS VOWEL");
+System.out.println("\n\n\t\t\t==============");break;
+}
+
+case 'A':
+{
+System.out.println("\n\n\t\t\tTHIS IS VOWEL");
+System.out.println("\n\n\t\t\t==============");break;
+}
+case 'E':
+{
+System.out.println("\n\n\t\t\tTHIS IS VOWEL");
+System.out.println("\n\n\t\t\t==============");break;
+}
+case 'I':
+{
+System.out.println("\n\n\t\t\tTHIS IS VOWEL");
+System.out.println("\n\n\t\t\t==============");break;
+}
+case 'O':
+{
+System.out.println("\n\n\t\t\tTHIS IS VOWEL");
+System.out.println("\n\n\t\t\t==============");break;
+}
+case 'U':
+{
+System.out.println("\n\n\t\t\tTHIS IS VOWEL");
+System.out.println("\n\n\t\t\t==============");break;
+}
+default:
+{
+System.out.println("PLEASE ENTER CHARCTER ONLY");
+break;
+}
+}
+}
+}
+

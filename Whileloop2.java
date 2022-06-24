2294f9f47c335500e3bca267edbb4c6331b237ea
Swamiom3 <106953535+Swamiom3@users.noreply.github.com>
2022-06-24 11:39:28 +0530

    Add files via upload

---
+++ Whileloop2.java
@@ -0,0 +1,20 @@
+class Whileloop2
+       {
+
+                        
+                 public static void main(String args[])
+                   { 
+
+                      int i=1;
+                       System.out.println("\n\t\tNUMBER \t\t\tSQUARE  ");
+                        while(i<=20) 
+                          {
+                               
+                                 System.out.print("\n\t\t"+i+"\n"+"\t\t\t\t\t"+(i*i));
+                            
+                          i++;
+
+                           }
+                          
+                     }
+         }

8f9aa9a978bcd35b968bb779af71bb7d23135da6
Swamiom3 <106953535+Swamiom3@users.noreply.github.com>
2022-06-24 12:43:57 +0530

    Add files via upload
    this is count sum of square.

---
+++ Countsqaure.java
@@ -0,0 +1,26 @@
+class Countsqaure
+       {
+
+                        
+                 public static void main(String args[])
+                   { 
+
+                      int i=1;int s=0;
+                       System.out.println("\n\t\tNUMBER  \t\tSQUARE  ");
+                        while(i<=10) 
+
+                          
+                            
+                          {
+                            s=s+i*i;
+                               
+                                 System.out.print("\n\t\t"+i+" "+"\t\t\t"+(i*i));
+                            
+                          i++;
+
+                           }
+                               System.out.println("\n\n\t\t=============================");
+                               System.out.println("\n\n\t\tTHIS IS THE SUM OF NUMBER:-"+s);
+                          
+                     }
+         }

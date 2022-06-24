6be17268df2cfe47ad5980a14fd43b060640ab39
Swamiom3 <106953535+Swamiom3@users.noreply.github.com>
2022-06-24 11:22:36 +0530

    Add files via upload

---
+++ Whileloop.java
@@ -0,0 +1,20 @@
+class Whileloop
+       {
+
+                        
+                 public static void main(String args[])
+                   { 
+
+                      int i;
+                       i=22;  
+                        while(i<=400) 
+                          {
+                                if(i%11==0)
+                                 System.out.print(i+" ");
+                            
+                          i++;
+
+                           }
+                          
+                     }
+         }
